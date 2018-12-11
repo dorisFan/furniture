beec342f3138f877e085a3343d7c75f4a02e2b62
dora06270627 <dora06270627@hotmail.com>
2018-12-11 14:42:59 -0500

    V1.0

--- src/app/furniture/furniture.component.ts
+++ src/app/furniture/furniture.component.ts
@@ -1,32 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Furniture, Obj } from '../shared/models/furniture';
-import { FurnitureService } from '../services/furniture.service';
+import { Component, OnInit } from "@angular/core";
+import { Furniture, Obj } from "../shared/models/furniture";
+import { FurnitureService } from "../services/furniture.service";
 
 @Component({
-  selector: 'app-furniture',
-  templateUrl: './furniture.component.html',
-  styleUrls: ['./furniture.component.css']
+  selector: "app-furniture",
+  templateUrl: "./furniture.component.html",
+  styleUrls: ["./furniture.component.css"]
 })
 export class FurnitureComponent implements OnInit {
-
   obj: Obj;
   furnitures: Furniture[];
 
-  constructor(private furnitureService : FurnitureService) { }
+  constructor(private furnitureService: FurnitureService) {}
 
   ngOnInit() {
     this.getAllFurnitures();
   }
 
   getAllFurnitures() {
-    this.furnitureService.getFurnitures()
-      .subscribe(
-        m => {
-          console.log(typeof m);
-          this.obj = m;
-          this.furnitures = this.obj.groups;
-          console.log(this.furnitures);
-        }
-      )
+    this.furnitureService.getFurnitures().subscribe(m => {
+      console.log(typeof m);
+      this.obj = m;
+      this.furnitures = this.obj.groups;
+      console.log(this.furnitures);
+    });
   }
 }
